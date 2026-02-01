1b610cd46be84805e0b24a92d5bd64e218c3f211
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2026-02-01 07:40:45 +0000

    add script to start canvas for render deployment testing

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd canvas-lms
+docker compose start postgres
+docker compose up -d
+bundle exec rails server
+
+
+# start_canvas="docker compose exec web bundle exec rails server"
+# start_postgres"docker compose start postgres"
+# stop_postgres="docker compose stop postgres"
+# restart_postgres="docker compose restart postgres"
